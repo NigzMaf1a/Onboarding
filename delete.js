e49d8462091e7540b6b5dfb455b2a01f09d682a7
Nigel Khasiani <118476871+NigzMaf1a@users.noreply.github.com>
2025-05-23 09:26:15 +0300

    Add files via upload

---
+++ delete.js
@@ -0,0 +1,28 @@
+import Product from './product.js';
+import { jsonData } from './products.js';
+
+const deleteSearchForm = document.getElementById("deleteSearchForm");
+const deleteProductID = document.getElementById("deleteProductId");
+const deleteBtn = document.getElementById("deleteBtn");
+
+deleteBtn.addEventListener("click", async () => {
+    const id = deleteProductID.value;
+
+    if (!id) {
+        alert("Please enter a Product ID.");
+        return;
+    }
+
+    try {
+        const product = new Product(jsonData);
+        await product.deleteProduct(id);
+
+        alert("Product deleted successfully!");
+        deleteSearchForm.reset();
+
+        history.replaceState(null, "", window.location.pathname);
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        alert("Product deletion failed: " + error.message);
+    }
+});

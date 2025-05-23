6ad7f95ad857ee16218b30b2bb70402dcfbf22ca
Nigel Khasiani <118476871+NigzMaf1a@users.noreply.github.com>
2025-05-23 09:29:39 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,164 @@
+export const jsonData = `{
+  "data": [
+    {
+      "id": 1,
+      "brand": "Dell",
+      "name": "XPS 13",
+      "price": 150000,
+      "description": "Laptop",
+      "image": "https://www.dell.com/sites/csimages/Video_Imagery/all/xps-13-9305-laptop.jpg"
+    },
+    {
+      "id": 2,
+      "brand": "HP",
+      "name": "Spectre x360",
+      "price": 140000,
+      "description": "Laptop",
+      "image": "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06947794.png"
+    },
+    {
+      "id": 3,
+      "brand": "Asus",
+      "name": "ROG Zephyrus G14",
+      "price": 130000,
+      "description": "Laptop",
+      "image": "https://dlcdnwebimgs.asus.com/gain/5C93C01E-7F4D-4A4C-8145-8C59489984C0/w717/h358"
+    },
+    {
+      "id": 4,
+      "brand": "Lenovo",
+      "name": "ThinkPad X1 Carbon",
+      "price": 160000,
+      "description": "Laptop",
+      "image": "https://www.lenovo.com/medias/lenovo-laptop-thinkpad-x1-carbon-gen-9-feature-6.png"
+    },
+    {
+      "id": 5,
+      "brand": "Apple",
+      "name": "MacBook Air M2",
+      "price": 200000,
+      "description": "Laptop",
+      "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-spacegray-select-202206_GEO_EMEA?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1653493200207"
+    },
+    {
+      "id": 6,
+      "brand": "Dell",
+      "name": "Inspiron 15 5000",
+      "price": 90000,
+      "description": "Laptop",
+      "image": "https://i.dell.com/sites/csimages/Master_Imagery/all/inspiron-15-5593-laptop-campaign-hero-504x350-ng.psd"
+    },
+    {
+      "id": 7,
+      "brand": "HP",
+      "name": "Envy 13",
+      "price": 115000,
+      "description": "Laptop",
+      "image": "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06629790.png"
+    },
+    {
+      "id": 8,
+      "brand": "Asus",
+      "name": "ZenBook 14",
+      "price": 125000,
+      "description": "Laptop",
+      "image": "https://dlcdnwebimgs.asus.com/gain/15f407db-18b1-435a-8c1a-1521a2f30c1a/"
+    },
+    {
+      "id": 9,
+      "brand": "Lenovo",
+      "name": "Yoga Slim 7",
+      "price": 130000,
+      "description": "Laptop",
+      "image": "https://www.lenovo.com/medias/lenovo-laptop-yoga-slim-7-feature-1.png"
+    },
+    {
+      "id": 10,
+      "brand": "Apple",
+      "name": "MacBook Pro 14",
+      "price": 250000,
+      "description": "Laptop",
+      "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp14-spacegray-select-202110?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1632788573000"
+    },
+    {
+      "id": 11,
+      "brand": "Samsung",
+      "name": "Galaxy S23 Ultra",
+      "price": 120000,
+      "description": "Phone",
+      "image": "https://images.samsung.com/is/image/samsung/p6pim/levant/sm-s918bzkgmea/gallery/levant-galaxy-s23-ultra-s918-446449-sm-s918bzkgmea-530504774"
+    },
+    {
+      "id": 12,
+      "brand": "Nokia",
+      "name": "X20",
+      "price": 40000,
+      "description": "Phone",
+      "image": "https://www.nokia.com/phones/sites/default/files/styles/scale_1440/public/products/nokia-x20-front_0.png"
+    },
+    {
+      "id": 13,
+      "brand": "Apple",
+      "name": "iPhone 14 Pro",
+      "price": 130000,
+      "description": "Phone",
+      "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-1-202209?wid=5120&hei=2880&fmt=jpeg&qlt=80&.v=1660753619946"
+    },
+    {
+      "id": 14,
+      "brand": "Oppo",
+      "name": "Find X5 Pro",
+      "price": 90000,
+      "description": "Phone",
+      "image": "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/find-x5-pro-en/FindX5Pro-glassBack-black-600.png"
+    },
+    {
+      "id": 15,
+      "brand": "Xiaomi",
+      "name": "12 Pro",
+      "price": 85000,
+      "description": "Phone",
+      "image": "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-12-pro/specs-header.png"
+    },
+    {
+      "id": 16,
+      "brand": "Tecno",
+      "name": "Phantom X2",
+      "price": 65000,
+      "description": "Phone",
+      "image": "https://www.tecno-mobile.com/sites/default/files/2022-09/Phantom-X2.png"
+    },
+    {
+      "id": 17,
+      "brand": "Samsung",
+      "name": "Galaxy A14",
+      "price": 30000,
+      "description": "Phone",
+      "image": "https://images.samsung.com/is/image/samsung/p6pim/africa/sm-a145fzkgxar/gallery/africa-galaxy-a14-a145-sm-a145fzkgxar-534134517?$720_576_PNG$"
+    },
+    {
+      "id": 18,
+      "brand": "Nokia",
+      "name": "G400",
+      "price": 35000,
+      "description": "Phone",
+      "image": "https://www.nokia.com/phones/sites/default/files/styles/scale_1440/public/products/nokia-g400-front_0.png"
+    },
+    {
+      "id": 19,
+      "brand": "Oppo",
+      "name": "Reno 8",
+      "price": 70000,
+      "description": "Phone",
+      "image": "https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/reno8-pro-en/reno8-pro-blue-600.png"
+    },
+    {
+      "id": 20,
+      "brand": "Xiaomi",
+      "name": "Redmi Note 12 Pro",
+      "price": 40000,
+      "description": "Phone",
+      "image": "https://i01.appmifile.com/webfile/globalimg/products/pc/redmi-note-12-pro/specs-header.png"
+    }
+  ]
+}`;

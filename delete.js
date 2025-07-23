26e2393936f8812328416e6d656940fefa7b98a6
Nigel Khasiani <118476871+NigzMaf1a@users.noreply.github.com>
2025-07-23 16:22:31 +0300

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,6 +1,6 @@
 import Product from './product.js';
 import { jsonData } from './products.js';
-////////////
+//////////////
 
 const deleteSearchForm = document.getElementById("deleteSearchForm");
 const deleteProductID = document.getElementById("deleteProductId");
